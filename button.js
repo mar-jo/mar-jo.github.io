00745ce84547d9c54aafcdce8c07ff4015242fdb
marjo <markx931@gmail.com>
2022-11-22 20:53:24 +0100

    help

--- button.js
+++ button.js
@@ -1,5 +1,8 @@
-const Web3 = require('Web3');
-var web3 = new Web3('https://goerli.etherscan.io');
+//const Web3 = require('Web3');
+const web3 = new Web3(new Web3.providers.HttpProvider("http://mar-jo.github.io"));
+
+//var web3 = new Web3('https://goerli.etherscan.io');
+
 
 const button = document.getElementById('button');
 button.addEventListener('click', () => {
