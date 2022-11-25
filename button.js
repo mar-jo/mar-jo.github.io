007e5256d46efe7615b0430b84ba98f4d13a7407
marjo <markx931@gmail.com>
2022-11-25 21:51:18 +0100

    Big update

--- button.js
+++
@@ -1,8 +0,0 @@
-let provider = new ethers.providers.Web3Provider(window.ethereum)
-let signer
-
-async function connectMetamask() { 
-    await provider.send("eth_requestAccounts", []);
-    signer = provider.getSigner();
-    console.log("Connected to Metamask with Address: " + signer._address);
-}
